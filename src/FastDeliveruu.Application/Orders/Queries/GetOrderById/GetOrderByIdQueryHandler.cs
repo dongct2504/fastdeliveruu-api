653be36cc682f6ab5583d354cf438d95a7348a9f
Cay Trieu Dong <dongct2504@gmail.com>
2024-10-14 15:22:08 +0700

    refactor: order  by date

--- src/FastDeliveruu.Application/Orders/Queries/GetOrderById/GetOrderByIdQueryHandler.cs
+++ src/FastDeliveruu.Application/Orders/Queries/GetOrderById/GetOrderByIdQueryHandler.cs
@@ -28,9 +28,11 @@ public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, Resul
     {
         OrderHeaderDetailDto? orderHeaderDetailDto = await _dbContext.Orders
             .Where(o => o.AppUserId == request.UserId && o.Id == request.OrderId)
+            .OrderByDescending(o => o.OrderDate)
             .AsNoTracking()
             .ProjectToType<OrderHeaderDetailDto>()
             .FirstOrDefaultAsync(cancellationToken);
+
         if (orderHeaderDetailDto == null)
         {
             string message = "Order not found.";
